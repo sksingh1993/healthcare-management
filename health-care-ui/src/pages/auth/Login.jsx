5fc628eee4b031cdf1032e0cfccb44d738b47544
Suraj Singh <kumarsurajsingh93@gmail.com>
2026-07-05 17:01:39 +0530

    Login background added

--- health-care-ui/src/pages/auth/Login.jsx
+++ health-care-ui/src/pages/auth/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { login as loginService } from "../../services/authService";
 import { useAuth } from "../../context/AuthContext";
 import LoginForm from "../../components/auth/LoginForm";
+import { Box, Card, CardMedia } from "@mui/material";
 
 export default function Login() {
 
@@ -71,14 +72,36 @@ export default function Login() {
 
     return (
 
-        <LoginForm
-            credentials={credentials}
-            setCredentials={setCredentials}
-            errors={errors}
-            setErrors={setErrors}
-            loading={loading}
-            onSubmit={handleSubmit}
-        />
+        <Box
+            sx={{
+                backgroundImage: "url('/heart_with_name2.png')", // put your image path here
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%", // stretches to fill width & height
+                backgroundPosition: "center",
+                height: "100vh",
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+                paddingTop:"5vh",
+                alignItems: "center",
+            }}
+        >
+
+            
+                <LoginForm
+                credentials={credentials}
+                setCredentials={setCredentials}
+                errors={errors}
+                setErrors={setErrors}
+                loading={loading}
+                onSubmit={handleSubmit}
+            />
+           
+
+        </Box>
+
+
+
 
     );
 
